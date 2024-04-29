d44e3e4bdedd1807a2121815078b82cbec3c0e21
caueoliveiraaa <leo.siscobra@gmail.com>
2024-04-29 20:14:45 -0300

    Update
    Update

--- csharp/hello-world/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ csharp/hello-world/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86e682dd78625c529ed706ed76a8a5ae0afc1154")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd87d7141af8c1116ec3487a61231cc61df577c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
