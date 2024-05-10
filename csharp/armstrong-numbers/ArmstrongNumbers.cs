0f25d734949583103d6999b2af3a3a10beb940a5
caueoliveiraaa <leo.siscobra@gmail.com>
2024-05-09 21:03:07 -0300

    Setup
    Setting up the next C# project.

---
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -0,0 +1,10 @@
+using System;
+
+public static class ArmstrongNumbers
+{
+    public static bool IsArmstrongNumber(int number)
+    {
+        string digits = number.ToString().ToArray();
+        Console.WriteLine("--", number)
+    }
+}
