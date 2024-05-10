56410b78c26819e0cae792c6d4be1c796862bcd0
caueoliveiraaa <leo.siscobra@gmail.com>
2024-05-09 22:17:51 -0300

    Resolution
    feat: resolution of Armstrong Numbers exercise.

--- csharp/armstrong-numbers/ArmstrongNumbers.cs
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -1,10 +1,34 @@
 using System;
+using System.Collections.Generic;
 
 public static class ArmstrongNumbers
 {
     public static bool IsArmstrongNumber(int number)
     {
-        string digits = number.ToString().ToArray();
-        Console.WriteLine("--", number)
+        string digits = number.ToString();
+        List<int> armstrongNumbers = GetArmstrongNumbers(digits);
+        int result = CalculateArmstrongNumbers(armstrongNumbers);
+        return result == number;
+    }
+
+    public static List<int> GetArmstrongNumbers(string digits) 
+    {
+        List<int> digitsList = new List<int>();
+        foreach (char digit in digits)
+            digitsList.Add(int.Parse(digit.ToString()));
+        return digitsList;
+    }
+
+    public static int CalculateArmstrongNumbers(List<int> armstrongNumbers)
+    {
+        int sum = 0;
+        foreach (var number in armstrongNumbers)
+        {
+            int powerOf = number;
+            for (int i = 1; i < armstrongNumbers.Count; i++)
+                powerOf *= number;
+            sum += powerOf;
+        }
+        return sum;
     }
 }
